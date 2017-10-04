4f359f3993143f214f783a7833e8fa493737a5a0
Mark <miglehart@yahoo.com>
2017-10-04 14:55:31 -0700

    Made a map on the index page.

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import csv
 from flask import Flask
 from flask import render_template
+from flask import abort
+from os.path import join
+
 app = Flask(__name__)
 
 def get_csv():
-    csv_path = '.\static\la-riots-deaths.csv'
+    csv_path = join('static','la-riots-deaths.csv')
     csv_file = open(csv_path, 'rb')
     csv_obj = csv.DictReader(csv_file)
     csv_list = list(csv_obj)
@@ -23,7 +26,7 @@ def detail(row_id):
     for row in object_list:
         if row['id'] == row_id:
             return render_template(template, object=row)
-
+    abort(404)
 
 if __name__ == "__main__":
     app.run(debug=True, use_reloader=True)
